629ebd4317ca3a329c55bc1a41f49ec2449b9e56
unknown <freekodelka86@gmail.com>
2023-04-15 21:22:59 +0300

    Решены задачи 19, 21

---
+++ Task19/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Напишите программу, которая принимает на вход 
+пятизначное число и проверяет, является ли оно палиндромом.*/
+string number;
+
+void readInput()
+{
+    Console.WriteLine("Enter number: ");
+    number = Console.ReadLine();
+}
+
+void outputResult(bool result)
+{
+    if (result)
+    {
+        Console.WriteLine("Число - палиндром");  
+    }
+    else
+    {
+        Console.WriteLine("Число - не является палиндромом");
+    }
+}
+
+bool isPalindrom(string index)
+{
+    if (index[0] == index[4] && index[1] == index[3])
+        return true;
+    return false;
+}
+
+readInput();
+outputResult(isPalindrom(number));
+
+ 
+
---
+++ Task21/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек 
+и находит расстояние между ними в 3D пространстве.*/
+
+Console.WriteLine ("Insert x1: ");
+double x1 = double.Parse (Console.ReadLine());
+Console.WriteLine ("Insert x2: ");
+double x2 = double.Parse (Console.ReadLine());
+Console.WriteLine ("Insert y1: ");
+double y1 = double.Parse (Console.ReadLine());
+Console.WriteLine ("Insert y2: ");
+double y2 = double.Parse (Console.ReadLine());
+Console.WriteLine ("Insert z1: ");
+double z1 = double.Parse (Console.ReadLine());
+Console.WriteLine ("Insert z2: ");
+double z2 = double.Parse (Console.ReadLine());
+Console.WriteLine (Math.Sqrt(Math.Pow((x1-x2),2) + Math.Pow((y1-y2),2) + Math.Pow((z1-z2),2)));
+
+
+
